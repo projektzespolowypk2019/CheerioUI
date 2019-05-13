ea1d0beac9ad6150ad69806b498dccdf7237fd51
Kamil Kopacz <k.dasch@interia.pl>
2019-05-13 19:39:42 +0200

    added path for add-recipe component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { HomeRoute } from './home.route';
+import { AddRecipeComponent } from './pages/add-recipe/add-recipe.component';
+
 const homeRoutes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path: HomeRoute.ADD,
+    component: AddRecipeComponent
   }
 ];
 
