4a9ce4525405f5a1e126626afb6a78308319615b
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-24 23:46:08 +0200

    Routes

---
+++ src/app/core/http/api-routes.ts
@@ -0,0 +1,15 @@
+export const ingredients = {
+  uri: '/ingredients'
+};
+
+export const recipes = {
+  uri: '/recipes'
+};
+
+export const units = {
+  uri: '/units'
+};
+
+export const users= {
+  uri: '/users'
+};
