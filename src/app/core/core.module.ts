8d2980f098cfd631419808b798ea4ff1f9782550
Kamil Kopacz <k.dasch@interia.pl>
2019-05-30 00:40:01 +0200

    added http services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MockDataService } from './services/mock-data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
-import {IngredientService} from "./http/ingredient/ingredient.service";
-import {RecipeService} from "./http/recipe/recipe.service";
-import {UnitService} from "./http/unit/unit.service";
-import {UserService} from "./http/user/user.service";
+import { IngredientService } from './http/ingredient/ingredient.service';
+import { RecipeService } from './http/recipe/recipe.service';
+import { UnitService } from './http/unit/unit.service';
+import { UserService } from './http/user/user.service';
+import { HeadersService } from './http/headers/headers.service';
 
 @NgModule({
   declarations: [],
@@ -14,11 +14,11 @@ import {UserService} from "./http/user/user.service";
     SharedModule,
   ],
   providers: [
-    MockDataService,
     IngredientService,
     RecipeService,
     UnitService,
-    UserService
+    UserService,
+    HeadersService
   ]
 })
 export class CoreModule { }
